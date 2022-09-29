b5591ce4650c9b8e2780e735129f170abbea4fc8
Khajamoinoddin <105960046+Khajamoinoddin@users.noreply.github.com>
2022-09-29 19:10:01 +0530

    navfoot cmpleted

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import {navbar} from "../html/components/nav.js"
+import {footer} from "../html/components/foot.js"
+
+
+document.getElementById("navbar").innerHTML=navbar();
+
+document.getElementById("footer").innerHTML=footer();
+
