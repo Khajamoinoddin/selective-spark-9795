8c9b2e7dd5f31ab1795f77b3e19d378f10d016b8
jitendra4545 <jitendra45.js45@gmail.com>
2022-09-30 22:53:11 +0530

    added

--- schedulepage.js
+++ schedulepage.js
@@ -1,16 +1,18 @@
 let container=document.getElementById('container')
 
-let upcoming=async()=>{
-    // alert('hi i am upcoming')
-      let date2=new Date
-      console.log(date2)
-      let month=date2.getUTCMonth()+1
-      let day=date2.getDate()
-      let year=date2.getFullYear()
 
-      
-      let month2=date2.getMonth()+2
-      
+let upcoming=async()=>{
+    //  alert('hi i am upcoming')
+    let date2=new Date
+    console.log(date2)
+    let month=date2.getUTCMonth()+1
+    let day=date2.getDate()
+    let year=date2.getFullYear()
+    
+    
+    let month2=date2.getMonth()+2
+    
+    
       console.log(year+"-"+month2+"-"+day)
       console.log(year+"-"+month+"-"+day)
       let res=await fetch(`https://api.api-cricket.com/cricket/?method=get_events&APIkey=7d62c723b884900b164dedced277c45adbaeaf5a56bf15301f9e17b4537c62a7&date_start=${(year+"-"+month+"-"+day)}&date_stop=${(year+"-"+month2+"-"+day)}`)
@@ -23,27 +25,29 @@ let upcoming=async()=>{
 upcoming()
 let appendData=(data)=>{
         container.innerHTML=null
-    data.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_team_logo,country_name,event_date_start,
-        event_status_info,event_status,event_time,league_name,league_season})=>{
-            if(event_status_info==null){  
+    data.forEach((el)=>{
+            if(el.event_status_info==null){  
             let div=document.createElement('div')
             div.id="main"
+            div.onclick=()=>{
+               appendupcoming(el)
+            }
             let div4=document.createElement('div')
             div4.id='cont'
             let div2=document.createElement('div')
            div2.className="team"
            
             let img=document.createElement('img')
-            img.src=event_home_team_logo
+            img.src=el.event_home_team_logo
              img.className='img1'
             let img2=document.createElement('img')
-            img2.src=event_away_team_logo
+            img2.src=el.event_away_team_logo
             img2.className='img1'
             let h2=document.createElement('h2')
-            h2.innerText=league_name+" "+league_season
+            h2.innerText=el.league_name+" "+el.league_season
             
             let p=document.createElement('p')
-            p.innerText=event_date_start
+            p.innerText=el.event_date_start
             p.className="time"
             let a=document.createElement('p')
         //  a.innerText=event_status_info
@@ -52,11 +56,11 @@ let appendData=(data)=>{
             //  div2.append(h2)
 
            let time=document.createElement('p')
-           time.innerText=event_time +"PM IST"
+           time.innerText=el.event_time +"PM IST"
              time.className="time"
 
            let h3=document.createElement('h3')
-           if(league_name=="ICC World Twenty20"||league_name=="One Day International"||league_name=="Twenty20 International"||league_name=="Twenty20 International Women"||league_name=="T20 Asia Cup Women"){
+           if(el.league_name=="ICC World Twenty20"||el.league_name=="One Day International"||el.league_name=="Twenty20 International"||el.league_name=="Twenty20 International Women"||el.league_name=="T20 Asia Cup Women"){
             h3.innerText="INT"
            }else{
             h3.innerText="DOM"
@@ -65,17 +69,17 @@ let appendData=(data)=>{
             let div3=document.createElement('div')
             div3.className="team"
                let team=document.createElement('h4')
-               team.innerText=event_home_team
+               team.innerText=el.event_home_team
                team.className="one" 
                let team2=document.createElement('h4')
-               team2.innerText=event_away_team
+               team2.innerText=el.event_away_team
                team2.className="one"
              let div5=document.createElement('div')
            let div6=document.createElement('div')
              div6.id="inter"
             let div7=document.createElement('div')
             let country=document.createElement('p')
-            country.innerText="Venue:- "+ country_name
+            country.innerText="Venue:- "+ el.country_name
 
             div3.append(team,team2)
             
@@ -93,9 +97,38 @@ let appendData=(data)=>{
 
 }
 
+let appendupcoming=(el)=>{
+  let arr=[]
+  arr.push(el)
+  
+  localStorage.setItem('data',JSON.stringify(arr))
+  window.location.href="upcoming.html"
+}
+
+// let upcomingData=JSON.parse(localStorage.getItem('data'))||[]
+// console.log(upcomingData)
+
+// let appendupcomingdata=()=>{
+//   upcomingData.forEach((el)=>{
+//     let div=document.createElement('div')
+
+//     let p=document.createElement('p')
+//      p.innerText=el.event_home_team
+      
+//      div.append(p)
+//      container.append(div)
+//   })
+// }
+
+
+
+
+
+
+
 
 let live=async()=>{
-
+// alert('hi i am live')
 
     let res=await fetch(`https://apiv2.api-cricket.com/?method=get_livescore&APIkey=7d62c723b884900b164dedced277c45adbaeaf5a56bf15301f9e17b4537c62a7`)
 
@@ -109,12 +142,13 @@ let live=async()=>{
 
 let appendlive=(data2)=>{
     container.innerHTML=null
-data2.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_team_logo,country_name,event_date_start,
-    event_status_info,event_status,event_time,league_name,league_season,event_stadium,event_service_away
-  ,event_home_final_result,event_away_final_result,event_service_home,event_type})=>{
-      if(event_status=="In Progress"){ 
+data2.forEach((el)=>{
+      if(el.event_status=="In Progress"){ 
         let div=document.createElement('div')
         div.id="main"
+        div.onclick=()=>{
+          appendliveData(el)
+       }
         let div4=document.createElement('div')
         div4.id='cont1'
         let div2=document.createElement('div')
@@ -124,43 +158,43 @@ data2.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_
         
         
         img.className='img1'
-        if(event_service_away===""){
-          img.innerText=event_away_final_result
+        if(el.event_service_away===""){
+          img.innerText=el.event_away_final_result
         }else{
-          img.innerText=event_away_final_result+" & "+event_service_away
+          img.innerText=el.event_away_final_result+" & "+el.event_service_away
         }
         let img2=document.createElement('p')
         
-        if(event_service_home===""){
-          img2.innerText=event_home_final_result
+        if(el.event_service_home===""){
+          img2.innerText=el.event_home_final_result
         }else{
-          img2.innerText=event_home_final_result+" & "+event_service_home
+          img2.innerText=el.event_home_final_result+" & "+el.event_service_home
         }
         img2.className='img1'
         let h2=document.createElement('h3')
-        h2.innerText=league_name+" "+league_season
+        h2.innerText=el.league_name+" "+el.league_season
         
         let p=document.createElement('p')
-        p.innerText=event_date_start
+        p.innerText=el.event_date_start
         p.className="time"
         let a=document.createElement('p')
     //    a.innerText=event_status_info
 
-        if(event_status_info==null){
+        if(el.event_status_info==null){
             a.innerText='Yet to start'
         
         }else{
-            a.innerText=event_status_info
+            a.innerText=el.event_status_info
         }
         //  div2.append(h2)
 
        let time=document.createElement('p')
-       time.innerText=event_time +"PM IST"
+       time.innerText=el.event_time +"PM IST"
          time.className="time"
 
        let h3=document.createElement('h3')
 
-       if(league_name=="ICC World Twenty20"||league_name=="One Day International"||league_name=="Twenty20 International"||league_name=="Twenty20 International Women"||league_name=="T20 Asia Cup Women"){
+       if(el.league_name=="ICC World Twenty20"||el.league_name=="One Day International"||el.league_name=="Twenty20 International"||el.league_name=="Twenty20 International Women"||el.league_name=="T20 Asia Cup Women"){
         h3.innerText="INT"
        }else{
         h3.innerText="DOM"
@@ -169,20 +203,20 @@ data2.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_
         let div3=document.createElement('div')
         div3.className="team"
            let team=document.createElement('h4')
-           team.innerText=event_home_team
+           team.innerText=el.event_home_team
            team.className="one" 
            let team2=document.createElement('h4')
-           team2.innerText=event_away_team
+           team2.innerText=el.event_away_team
            team2.className="one"
          let div5=document.createElement('div')
        let div6=document.createElement('div')
          div6.id="inter"
         let div7=document.createElement('div')
         let country=document.createElement('p')
-        country.innerText="Venue:- "+event_stadium
+        country.innerText="Venue:- "+el.event_stadium
 
         let type=document.createElement('h4')
-        type.innerText=event_type
+        type.innerText=el.event_type
        type.style.color="orange"
         div3.append(team,team2)
 
@@ -202,20 +236,34 @@ data2.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_
 }
 
 
+
+
+
+let appendliveData=(el)=>{
+  alert('hi live')
+  let arr=[]
+  arr.push(el)
+  
+  localStorage.setItem('datalive',JSON.stringify(arr))
+  //  window.location.href="live.html"
+}
+
+
 let results=async()=>{
+  // alert('hi i am results')
   let date2=new Date
       console.log(date2)
-      let month=date2.getUTCMonth()+1
+      let month=date2.getMonth()+1
       let day=date2.getDate()-1
       let year=date2.getFullYear()
 
       
       let month2=date2.getMonth()
-      
+      //${(year+"-"+month2+"-"+day)}
       console.log(year+"-"+month2+"-"+day)
       console.log(year+"-"+month+"-"+day)
 
-      let res=await fetch(`https://api.api-cricket.com/cricket/?method=get_events&APIkey=7d62c723b884900b164dedced277c45adbaeaf5a56bf15301f9e17b4537c62a7&date_start=${(year+"-"+month2+"-"+day)}&date_stop=${(year+"-"+month+"-"+day)}`)
+      let res=await fetch(`https://api.api-cricket.com/cricket/?method=get_events&APIkey=7d62c723b884900b164dedced277c45adbaeaf5a56bf15301f9e17b4537c62a7&date_start=2022-09-26&date_stop=2022-10-03`)
       res=await res.json()
     
      let data3=res.result
@@ -230,38 +278,40 @@ appendresults(data3)
 
   let appendresults=(data3)=>{
     container.innerHTML=null
-data3.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_team_logo,country_name,event_date_start
-    ,event_status,event_time,league_name,league_season,event_stadium,event_away_final_result,event_home_final_result,event_status_info})=>{
-        if(event_status=="Finished"){  
+data3.forEach((el)=>{
+       if(el.event_status=="Finished"){  
         let div=document.createElement('div')
         div.id="main"
+        div.onclick=()=>{
+          appendresultsData(el)
+       }
         let div4=document.createElement('div')
         div4.id='cont'
         let div2=document.createElement('div')
        div2.className="team"
        
         let img=document.createElement('img')
-        img.src=event_home_team_logo
+        img.src=el.event_home_team_logo
          img.className='img1'
         let img2=document.createElement('img')
-        img2.src=event_away_team_logo
+        img2.src=el.event_away_team_logo
         img2.className='img1'
         let h2=document.createElement('h2')
-        h2.innerText=league_name+" "+league_season
+        h2.innerText=el.league_name+" "+el.league_season
         
         let p=document.createElement('p')
-        p.innerText=event_away_final_result
+        p.innerText=el.event_away_final_result
 
         p.className="time"
         let a=document.createElement('p')
-        a.innerText=event_status_info
+        a.innerText=el.event_status_info
        let time=document.createElement('p')
-       time.innerText=event_home_final_result
+       time.innerText=el.event_home_final_result
 
          time.className="time"
 
        let h3=document.createElement('h3')
-       if(league_name=="ICC World Twenty20"||league_name=="One Day International"||league_name=="Twenty20 International"||league_name=="Twenty20 International Women"||league_name=="T20 Asia Cup Women"||league_name=="Test Series"){
+       if(el.league_name=="ICC World Twenty20"||el.league_name=="One Day International"||el.league_name=="Twenty20 International"||el.league_name=="Twenty20 International Women"||el.league_name=="T20 Asia Cup Women"||el.league_name=="Test Series"){
         h3.innerText="INT"
        }else{
         h3.innerText="DOM"
@@ -270,10 +320,10 @@ data3.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_
         let div3=document.createElement('div')
         div3.className="team"
            let team=document.createElement('h4')
-           team.innerText=event_home_team
+           team.innerText=el.event_home_team
            team.className="one" 
            let team2=document.createElement('h4')
-           team2.innerText=event_away_team
+           team2.innerText=el.event_away_team
            team2.className="one"
          let div5=document.createElement('div')
        let div6=document.createElement('div')
@@ -292,7 +342,7 @@ data3.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_
       
       
        container.append(div)
-    }
+     }
 })
 
 
@@ -301,11 +351,32 @@ data3.forEach(({event_away_team,event_home_team,event_away_team_logo,event_home_
 
 
 
-
-let selectone=()=>{
-  let select=document.getElementById('inn2').value ;
-  console.log(select)
-
- 
+let appendresultsData=(el)=>{
+  
+  let arr=[]
+  arr.push(el)
+  
+  localStorage.setItem('dataresult',JSON.stringify(arr))
+ window.location.href="result.html"
 }
+
+// let selectone=()=>{
+//   let select=document.getElementById('inn2').value ;
+//   if(select=="INT"){
+//     console.log('i am interntional')
+//   }else if(select=='DOM'){
+//     console.log('i am domestic')
+//   }else{
+//     console.log('i am all')
+//   }
+  
+
+    
+   
+      
+//   appendData(filter_data)
+// }
+
+
+
 
--- upcoming.js
+++ upcoming.js
@@ -1,1 +1,21 @@
-let api="7d62c723b884900b164dedced277c45adbaeaf5a56bf15301f9e17b4537c62a7"
+//let api="7d62c723b884900b164dedced277c45adbaeaf5a56bf15301f9e17b4537c62a7"
+
+
+let container=document.getElementById('container')
+
+let upcomingData=JSON.parse(localStorage.getItem('data'))||[]
+console.log(upcomingData)
+
+let appendupcomingdata=()=>{
+  upcomingData.forEach((el)=>{
+    let div=document.createElement('div')
+
+    let p=document.createElement('p')
+     p.innerText=el.event_home_team
+      
+     div.append(p)
+     container.append(div)
+  })
+}
+
+appendupcomingdata()
